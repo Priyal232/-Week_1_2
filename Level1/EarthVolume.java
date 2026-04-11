995fc9365301eb951f341a86560cc6646789e12c
07pz1491 <pz1491@srmist.edu.in>
2026-04-11 15:31:06 +0530

    level2

--- Level1/EarthVolume.java
+++
@@ -1,9 +0,0 @@
-public class EarthVolume {
-    public static void main(String[] args) {
-        double r = 6378, pi = Math.PI;
-        double volKm3 = (4.0/3) * pi * Math.pow(r, 3);
-        double volMi3 = volKm3 / Math.pow(1.6, 3);
-        System.out.println("The volume of earth in cubic kilometers is " + volKm3 + 
-                          " and cubic miles is " + volMi3);
-    }
-}
